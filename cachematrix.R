66a7bead6f84d3f9ebac362302ab7b3d9aa3d782
Lawrence <lawrence-wong@live.com>
2014-08-24 00:06:25 -0400

    Add logic to placeholder functions: makeCacheMatrix and cachSolve; defined new S4 class 'mymatrix'

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a pair of functions that creates an object
+## that is a subclass of matrix, and enables the
+## computation and caching of its inverse
 
-## Write a short comment describing this function
+## This function defines a S4 class called "myMatrix" that 
+## is a subclass of matrix. The class myMatrix has a slot
+## that is intended for the caching of the matrix's inverse.
+##
+## Given a matrix, this function creates a an instance of
+## "myMatrix" and returns it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  setClass("myMatrix", representation(invertedMatrix="NULL"), contains="matrix");
+  return(new("myMatrix", x));
 }
 
 
-## Write a short comment describing this function
+## Given an instance of "myMatrix", this function returns
+## its inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if (is.null(x@invertedMatrix)) {
+    x@invertedMatrix = solve(x);
+  }
+  return x@invertedMatrix;
 }
